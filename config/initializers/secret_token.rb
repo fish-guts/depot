666162417d8e07453e0902ff89fc09f59b5efb2c
Severin Mueller <severin.mueller@gmx.net>
2012-05-14 22:56:42 +0200

    moved to postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '7a8d8a294a1d27cb677c5e9c5a5be08190d93d811c29eb8d1b40784edf4b8bbd4e9182b77bae1df10d43db6a8b9d36c657bed8a68bf0125c601cd6ddaf9098d8'
